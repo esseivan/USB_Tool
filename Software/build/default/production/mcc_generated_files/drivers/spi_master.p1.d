14c626eb6d5e6716e83ec0ea9f20465d91cda513
EsseivaN <nicolas.esseiva@gmail.com>
2019-06-17 23:52:03 +0200

    v4

--- Software/build/default/production/mcc_generated_files/drivers/spi_master.p1.d
+++
@@ -1,5 +0,0 @@
-build/default/production/mcc_generated_files/drivers/spi_master.p1:  \
-mcc_generated_files/drivers/spi_master.c  \
-mcc_generated_files/drivers/spi_master.h  \
-mcc_generated_files/drivers/../spi2_driver.h  \
-mcc_generated_files/drivers/../spi2_types.h 
--- Software/build/default/production/mcc_generated_files/spi2_driver.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/production/mcc_generated_files/spi2_driver.p1:  \
-mcc_generated_files/spi2_driver.c  \
-mcc_generated_files/spi2_driver.h  \
-mcc_generated_files/spi2_types.h 
